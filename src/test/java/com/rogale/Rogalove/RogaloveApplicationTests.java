55eca535d339ea579e41718227e55ccfff433004
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-03-07 16:11:47 +0100

    first commit

---
+++ src/test/java/com/rogale/Rogalove/RogaloveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rogale.Rogalove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogaloveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
