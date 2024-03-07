8e5f1dcfbe2f61ee4f15cae076d99326606449f1
Tomasz Pawliczak <tomasz.pawliczak@wundermanthompson.com>
2024-03-07 16:17:23 +0100

    first commit

---
+++ src/main/kotlin/com/rogale/Rogalove/RogaloveApplication.kt
@@ -0,0 +1,11 @@
+package com.rogale.Rogalove
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RogaloveApplication
+
+fun main(args: Array<String>) {
+	runApplication<RogaloveApplication>(*args)
+}
